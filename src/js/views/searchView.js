1fc94021b13f41d47728143ecc5c5a3a163e4e66
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2024-05-25 21:43:10 +0100

    Implement search result

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,8 +1,22 @@
 class SearchView {
   #parentEl = document.querySelector('.search');
 
-  getQuerry() {
-    return this.#parentEl.querySelector('.search_field').value;
+  getQuery() {
+    const query = this.#parentEl.querySelector('.search__field').value;
+    this.#clearInput();
+    return query;
+  }
+
+  #clearInput() {
+    this.#parentEl.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this.#parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+      // controlSearchResults(getQuery());
+    });
   }
 }
 
