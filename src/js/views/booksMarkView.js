08fc8697bec8bc5b516e77b229230c67aecdcb72
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2024-06-01 00:29:12 +0100

    Refactor bookmark

--- src/js/views/booksMarkView.js
+++ src/js/views/booksMarkView.js
@@ -1,5 +1,6 @@
+import previewView from './previewView';
 import View from './View';
-import icons from 'url:../../img/icons.svg';
+// import icons from 'url:../../img/icons.svg';
 
 class BookmarkView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
@@ -7,30 +8,9 @@ class BookmarkView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-    // this.render(data);
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-            <a class="preview__link ${
-              result.id === id ? 'preview__link--active' : ''
-            }" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-                
-              </div>
-            </a>
-    </li>
-           
-    `;
+    return this._data
+      .map(bookmarks => previewView.render(bookmarks, false))
+      .join('');
   }
 }
 
