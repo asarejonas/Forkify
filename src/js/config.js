3d8ef1ceadb40363e091fb43f4a34d3936f26dd7
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2024-06-03 22:24:16 +0100

    API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '0f33452f-a967-4c96-a83a-bb6206cc569c';
