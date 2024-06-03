454157e1d17a7668975c5cdf228a8b8ae4a3b5a7
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2024-06-03 18:20:23 +0100

    API key & modal timer

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '0f33452f-a967-4c96-a83a-bb6206cc569c';
+export const KEY = '39aa329f-6e19-4893-aca1-40aae60cbd53';
+export const MODAL_CLOSE_SEC = 2.5;
