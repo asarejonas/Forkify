dce94300c8d2db7d46b216b492c66c4e7620680a
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2024-06-06 12:48:56 +0100

    Documentation to render the function

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,6 +4,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   *  Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is return if render=false
+   * @this {Object} View object
+   * @author Jonas Asare
+   * @todo Finish implementation
+   */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
